ead0c248110e4cb5752053a76d16e660f8947503
AntonVoronezh <pstut@mail.ru>
2022-10-28 16:36:49 +0300

    4

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # uvicorn main:app --reload
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 from shemas import Book
 
@@ -11,8 +11,8 @@ def home():
     return {'key': 'hello'}
 
 
-@app.get('/{pk}')
-def get_item(pk: int, q: str = None):  # http://127.0.0.1:8000/7777?q=iouo
+@app.get('/1/{pk}')
+def get_item(pk: int, q: str = None):  # http://127.0.0.1:8000/1/7777?q=iouo
     return {'key': pk, 'q': q}
 
 
@@ -24,3 +24,13 @@ def get_user_item(pk: int, item: str):
 @app.post('/book')
 def create_book(book: Book):
     return book
+
+
+@app.get('/book') # http://127.0.0.1:8000/book?q=hhhh
+def get_book(q: str = Query(None, max_length=5, min_length=2, description='Это то-то и то-то')):
+# def get_book(q: str = Query(..., max_length=5) - обязательный параметр ...
+# def get_book(q: str = Query("test", max_length=5) - параметр по умолчанию
+# def get_book(q: List[str] = ) - лист значений
+# def get_book(q: List[str] = Query(["test", "test2"]) - лист значений с параметр по умолчанию
+# def get_book(q: str = Query(..., , deprecated=True) - устаревший параметр
+    return q
