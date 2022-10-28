8bd13b549cfe8a6352a7155d63024c291d906cba
AntonVoronezh <pstut@mail.ru>
2022-10-28 09:18:52 +0300

    2

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ app = FastAPI()
 def home():
     return {'key': 'hello'}
 
+
+@app.get('/{pk}')
+def get_item(pk: int, q: str = None): # http://127.0.0.1:8000/7777?q=iouo
+    return {'key': pk, 'q': q}
+
