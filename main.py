d2077f5c445337a3d7c0b776d84b49b575a20be9
AntonVoronezh <pstut@mail.ru>
2022-10-28 10:01:33 +0300

    3

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+# uvicorn main:app --reload
 from fastapi import FastAPI
+
+from shemas import Book
 
 app = FastAPI()
 
@@ -9,6 +12,15 @@ def home():
 
 
 @app.get('/{pk}')
-def get_item(pk: int, q: str = None): # http://127.0.0.1:8000/7777?q=iouo
+def get_item(pk: int, q: str = None):  # http://127.0.0.1:8000/7777?q=iouo
     return {'key': pk, 'q': q}
 
+
+@app.get('/users/{pk}/items/{item}')  # http://127.0.0.1:8000/users/2/items/tttgr
+def get_user_item(pk: int, item: str):
+    return {'pk': pk, 'item': item}
+
+
+@app.post('/book')
+def create_book(book: Book):
+    return book
---
+++ shemas.py
@@ -0,0 +1,17 @@
+from typing import List
+from pydantic import BaseModel
+from datetime import date
+
+
+class Genre(BaseModel):
+    name: str
+
+
+class Book(BaseModel):
+    title: str
+    writer: str
+    duration: str
+    date: date
+    summary: str = None
+    genres: List[Genre]
+    pages: int
