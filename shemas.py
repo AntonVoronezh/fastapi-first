2202896e97a5942a48ae1f5daddc04af645d302e
AntonVoronezh <pstut@mail.ru>
2022-10-28 18:19:25 +0300

    7

--- shemas.py
+++ shemas.py
@@ -1,5 +1,5 @@
 from typing import List
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from datetime import date
 
 
@@ -21,5 +21,11 @@ class Author(BaseModel):
     first_name: str
     last_name: str
     age: int
+
+    @validator('age')
+    def check_value(cls, v):
+        if v < 15:
+            raise ValueError('возраст не меньше 15')
+        return v
 
 
